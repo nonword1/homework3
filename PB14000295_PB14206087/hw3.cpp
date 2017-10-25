d2fd2e13a6d129b53c38ab359d4c8d437e4733cb
Shawnxm <26355425+Shawnxm@users.noreply.github.com>
2017-10-25 19:45:28 +0800

    Update hw3.cpp

--- PB14000295_PB14206087/hw3.cpp
+++ PB14000295_PB14206087/hw3.cpp
@@ -1,73 +1,5 @@
-#include "core.hpp"
-#include "highgui.hpp"
-#include "iostream"
-#include "hw3.h"
-#include "time.h"
-using namespace cv;
-using namespace std;
+#define MAX_LABEL 10000
 
-#define SHOW_IMG_IN_NUM
-
-int main()
-{
-	namedWindow("result");
-	Mat img = imread("pic5.jpg",0);//读取灰度图
-	int row = img.rows;
-	int col = img.cols;
-	int row_num;
-	Mat binaryImg(row, col, CV_8UC1);
-	Mat binaryImg2(row, col, CV_8UC1);
-	ustc_ConnectedComponentLabeling(img, binaryImg, 80);
-	int max = -1;
-	int index;
-	for (int i = 0; i < row; i++)
-	{
-		row_num = i * col;
-		for (int j = 0; j < col; j++)
-		{
-			index = row_num + j;
-			if (img.data[index] >= 125)
-			{
-				binaryImg2.data[index] = 200;
-			}
-			else
-			{
-				binaryImg2.data[index] = 0;
-			}
-		}
-
-	}
-	for (int i = 0; i < row; i++)
-	{
-		row_num = i * col;
-		for (int j = 1; j <= col; j++)
-		{
-			index = row_num + j;
-			if (max<binaryImg.data[index])
-			{
-				max = binaryImg.data[index];
-			}
-		}
-
-	}
-	for (int i = 0; i < row; i++)
-	{
-		row_num = i * col;
-		for (int j = 1; j <= col; j++)
-		{
-			index = row_num + j;
-		
-				binaryImg.data[index] = binaryImg.data[index] * 255 / max;
-
-			
-		}
-
-	}
-	imshow("result", binaryImg);
-	waitKey();
-}
-
-//sub function
 void resolve_label(int* labelset, int a, int b, int num)
 {
 	int min;
@@ -92,7 +24,6 @@ void resolve_label(int* labelset, int a, int b, int num)
 
 }
 
-//任务函数
 int ustc_ConnectedComponentLabeling(Mat grayImg, Mat& labelImg, int bin_th)
 {
 	if ((bin_th < 0) || (bin_th > 255))
@@ -180,7 +111,7 @@ int ustc_ConnectedComponentLabeling(Mat grayImg, Mat& labelImg, int bin_th)
 					{
 						a = labelImg.data[x1];
 						b = labelImg.data[x4];
-						resolve_label(labelset, a ,b, label);
+						resolve_label(labelset, labelset[a] ,labelset[b], label);
 					}
 				}
 				else if (c2)
@@ -190,7 +121,7 @@ int ustc_ConnectedComponentLabeling(Mat grayImg, Mat& labelImg, int bin_th)
 					{
 						a = labelImg.data[x2];
 						b = labelImg.data[x4];
-						resolve_label(labelset, a, b, label);
+						resolve_label(labelset, labelset[a], labelset[b], label);
 					}
 				}
 				else if (c4)
@@ -222,117 +153,4 @@ int ustc_ConnectedComponentLabeling(Mat grayImg, Mat& labelImg, int bin_th)
 
 	}
 	return MY_OK;
-}
-
-//测试函数
-void test_ConnectedComponentLabeling(Mat &grayImg)
-{
-	Mat labelImg(grayImg.rows, grayImg.cols, CV_32SC1);
-	int bin_th = 100;
-	int i = 0, j = 0;
-
-	time_t start = clock();
-	for (i = 100; i > 0; i--)
-	{
-		ustc_ConnectedComponentLabeling(grayImg, labelImg, bin_th);
-	}
-	time_t end = clock();
-	cout << (end - start) / 100.0f << "ms used per time\n";
-
-#ifdef SHOW_IMG_IN_NUM
-	cout << "\n\n";
-	cout << "---------------------------------------------------------------------------------------------------------------\n";
-	for (i = 0; i < labelImg.rows; i++)
-	{
-		for (j = 0; j < labelImg.cols; j++)
-		{
-			cout.width(2);
-			cout << ((int*)labelImg.data)[i * labelImg.cols + j];
-		}
-		cout << endl;
-	}
-#endif // SHOW_IMG_IN_NUM
-
-	cin >> i;
-}
-
-//灰度图二值化，二值图行列均多2
-void gray_expand2_binary(const Mat &grayImg, Mat &binaryImg, const int bin_th)
-{
-	int i = 0, j = 0;
-	int i_gray = 0, j_gray = 0;
-	int i_binary = 0, j_binary = 0;
-	int gray_row = grayImg.rows;
-	int gray_col = grayImg.cols;
-	int tmp_crt_row_gray = 0;
-	int tmp_crt_row_bi = 0;
-	int threshold[256] = { 0 };
-	//等于阈值为前景
-	for (i = 255; i >= bin_th; i--)
-	{
-		threshold[i] = 1;
-	}
-
-	int j_quotient = 0;
-	int j_remainder = 0;
-	for (i_gray = gray_row - 1, i_binary = gray_row; i_gray >= 0; i_gray--, i_binary--)
-	{
-		tmp_crt_row_bi = i_binary * binaryImg.cols;
-		tmp_crt_row_gray = i_gray * gray_col;
-		j_quotient = grayImg.cols >> 2;
-		j_remainder = grayImg.cols - (j_quotient << 2);
-		for (j_gray = gray_col - 1, j_binary = gray_col; j_quotient >= 0; j_quotient--)
-		{
-			binaryImg.data[tmp_crt_row_bi + j_binary] = threshold[grayImg.data[tmp_crt_row_gray + j_gray]];  j_gray--; j_binary--;
-			binaryImg.data[tmp_crt_row_bi + j_binary] = threshold[grayImg.data[tmp_crt_row_gray + j_gray]];  j_gray--; j_binary--;
-			binaryImg.data[tmp_crt_row_bi + j_binary] = threshold[grayImg.data[tmp_crt_row_gray + j_gray]];  j_gray--; j_binary--;
-			binaryImg.data[tmp_crt_row_bi + j_binary] = threshold[grayImg.data[tmp_crt_row_gray + j_gray]];  j_gray--; j_binary--;
-		}
-		for (; j_remainder >= 0; j_remainder--)
-		{
-			binaryImg.data[tmp_crt_row_bi + j_binary] = threshold[grayImg.data[tmp_crt_row_gray + j_gray]];  j_gray--; j_binary--;
-		}
-	}
-
-	//灰度图头尾两行置零
-	i = (binaryImg.rows - 1) * binaryImg.cols;
-	for (j = binaryImg.cols - 1; j >= 0; j--)
-	{
-		binaryImg.data[j] = 0;
-		binaryImg.data[i + j] = 0;
-	}
-
-	//灰度图头尾两列置零
-	j = binaryImg.cols - 1;
-	for (i = binaryImg.rows - 1; i >= 0; i--)
-	{
-		tmp_crt_row_bi = i * binaryImg.cols;
-		binaryImg.data[tmp_crt_row_bi] = 0;
-		binaryImg.data[tmp_crt_row_bi + j] = 0;
-	}
-}
-
-//
-void label_tmp_shrink2_label(const Mat &labelImg_tmp, Mat &labelImg)
-{
-	int i = 0, j = 0;
-	int i_tmpIm = 0, j_tmpIm = 0;
-	int row = labelImg.rows;
-	int col = labelImg.cols;
-	int row_tmpIm = labelImg_tmp.rows;
-	int col_tmpIm = labelImg_tmp.cols;
-	int mark = 0;
-	int crt_rowhead = 0;
-	int crt_rowhead_tmpIm = 0;
-
-	for (i = row - 1, i_tmpIm = row; i >= 0; i--, i_tmpIm--)
-	{
-		crt_rowhead = i * col;
-		crt_rowhead_tmpIm = (i + 1) * col_tmpIm;
-		for (j = col - 1; j >= 0; j--)
-		{
-			mark = ((int*)labelImg_tmp.data)[crt_rowhead_tmpIm + j + 1];
-			((int*)labelImg.data)[crt_rowhead + j] = mark;
-		}
-	}
 }
